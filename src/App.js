d333e27aeb9fa720dda266f4f403bb0347468ab5
fernando <crosio.fernando@gmail.com>
2022-10-03 09:05:06 -0300

    Concatenación de array mediante manejo de useState, recorriendo array con keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Lista from "./components/Lista";
+
 
 function App() {
-  return (
+  
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Lista</h1>
+      <Lista></Lista>
+      
     </div>
   );
 }
